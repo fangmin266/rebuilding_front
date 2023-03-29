6b9b7afe729ea4e445791bcd58513182a0461797
fangmin266 <fangmin226@gmail.com>
2023-03-29 15:47:28 +0900

    kakaologin - cors문제로 보류

--- src/screens/admin/components/Button.tsx
+++ src/screens/admin/components/Button.tsx
@@ -7,6 +7,6 @@ export interface ButtonDefualtProps{
   }
 export const ButtonDefault = ({title,bgcolor,txtcolor,btnWidth,onClickFunction}:ButtonDefualtProps) =>{
     return(
-      <button className={' rounded py-3 font-semibold '+bgcolor+' '+txtcolor+' '+(btnWidth?.includes('w-')?btnWidth:' w-full ' )}onClick={onClickFunction}>{title}</button>
+      <button  className={' rounded py-3 font-semibold '+bgcolor+' '+txtcolor+' '+(btnWidth?.includes('w-')?btnWidth:' w-full ' )}onClick={onClickFunction}>{title}</button>
     )
   }
--- src/screens/admin/components/SocialLoginBtn.tsx
+++ src/screens/admin/components/SocialLoginBtn.tsx
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import React from 'react'
 import { useDispatch } from 'react-redux'
 import { common } from '../../../common/api'
@@ -9,14 +10,19 @@ const SocialLoginBtn = () => {
   const social = ['구글','페북','애플']
   const dispatch = useDispatch<AppDispatch>()
   const onClickKakao = async() =>{
-    // document.form_chk.action = `${common.baseURL}base/kakaoLogin`
-    // document.form_chk.submit()
-    const res = await dispatch(kakaoLogin())
-    console.log(res,"res")
+  //  document.form_chk.action = `${common.baseURL}base/kakaoLogin`
+  //   document.form_chk.submit()
+
+    const res = await dispatch(kakaoLogin()).unwrap()
+   console.log(res,'res')
+     window.location.href = res.data.url; // 리다이렉트 URL로 이동
+
+
   }
   return (
     <>
-      <div className='flex flex-col gap-y-3 pt-3'>
+      <div
+       className='flex flex-col gap-y-3 pt-3'>
         <ButtonDefault title='카카오로 시작하기' bgcolor='bg-[#fae225]' txtcolor='text-black' onClickFunction={onClickKakao}/>
         <ButtonDefault title='네이버로 시작하기' bgcolor='bg-[#00d337]' txtcolor='text-white'/>
       </div>
